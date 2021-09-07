901a565e94ba484be79ab5208d4635aff64c1ab6
Kekelberi <alexei.gaponov@gmail.com>
2021-09-07 21:52:15 +0300

    modal

--- src/component/ModalWin.js
+++ src/component/ModalWin.js
@@ -1,36 +1,39 @@
 import React, { useState } from 'react'
 import { Button, Modal, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 export default function ModalWin({ back, visible, title, corect }) {
-const [corTitle, setCorTitle] = useState(title)
+  const [corTitle, setCorTitle] = useState(title)
 
-	return (
-            
-		<Modal visible={ visible }>
-			<TextInput
-        value={ corTitle}
+  return (
+
+    <Modal visible={ visible }>
+      <TextInput
+        value={ corTitle }
         onChangeText={ setCorTitle }
-				placeholder='text'
-				style={ styles.inp }
-       />
-			<View style={ styles.but } >
-				<Button title='back' onPress={ back } />
-        <Button title='corect' onPress={ () => corect(corTitle) } />
+        placeholder='text'
+        style={ styles.inp }
+      />
+      <View style={ styles.but } >
+        <Ionicons name='md-chevron-back-circle-sharp' size={ 35 } onPress={ back } />
+        <FontAwesome name="save" size={ 35 } color="black" onPress={ () => corect(corTitle) } />
 
-			</View>
-		</Modal>
-	)
+      </View>
+    </Modal>
+  )
 }
 
 const styles = StyleSheet.create({
-	inp: {
-		padding: 15,
-		backgroundColor: '#eee',
-		elevation: 11,
-		marginBottom: 11,
-	},
-	but: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	}
+  inp: {
+    padding: 15,
+    backgroundColor: '#eee',
+    elevation: 11,
+    marginBottom: 11,
+  },
+  but: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
 })
