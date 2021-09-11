ff8cd9f70b8cb76f6216cfa95e41daa3d483d08d
Kekelberi <alexei.gaponov@gmail.com>
2021-09-12 00:59:24 +0300

    save

--- src/context/screen/screenReducer.js
+++ src/context/screen/screenReducer.js
@@ -1,16 +1,6 @@
 import { CHANGE_SCREEN } from "../todo/type";
 
-//const handlers = {
-//	[CHANGE_SCREEN]: (state, payload) => payload,
-//	DEFAULT: state => state
-//}
 
-//export const screenReducer = (state, action) => {
-//	const handler = handlers[action.type] || handlers.DEFAULT
-//	return handler(state, action.payload)
-//}
-
-//import { CHANGE_SCREEN } from '../types'
 
 const handlers = {
 	[CHANGE_SCREEN]: (state, payload) => payload,
